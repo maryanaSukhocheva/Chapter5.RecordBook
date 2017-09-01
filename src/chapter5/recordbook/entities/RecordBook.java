c82c28f73797f754a6e70a28a492b29177a75386
SLAVA <SLAVA@SLAVAPC>
2017-09-01 09:36:21 +0600

    Исправлены замечания

--- src/chapter5/recordbook/entities/RecordBook.java
+++ src/chapter5/recordbook/entities/RecordBook.java
@@ -19,39 +19,39 @@ public class RecordBook {
         return fioStudent;
     }
     public void setFioStudent(String fioStudent){
-    this.fioStudent = fioStudent;
+        this.fioStudent = fioStudent;
     }
     public class Info{
         int numberSession,countOfNonHandedOffsets, countOfExamsForNextSession;
         public Info(){  
-        System.out.println("сессия №:");
-        setNumberSession(scanner.nextInt());    
-        System.out.println("количество не сданных зачётов:");
-        setCountOfNonHandedOffsets(scanner.nextInt());
-        System.out.println("количество экзаменов на сл. сессию:");
-        setСountOfExamsForNextSession(scanner.nextInt());    
-    }
-    public int getNumberSession(){
-    return numberSession;
-    }
-    public void setNumberSession(int number){
-    this.numberSession = number;
-    }
-    public int getCountOfNonHandedOffsets(){
-    return countOfNonHandedOffsets;
-    }
-    public void setCountOfNonHandedOffsets(int count){
-    this.countOfNonHandedOffsets = count;
-    }
-    public int getCountOfExamsForNextSession(){
-    return countOfExamsForNextSession;
-    }
-    public void setСountOfExamsForNextSession(int count){
-    this.countOfExamsForNextSession = count;
-    }
-    public void print(){
-        System.out.print("Студент по фамилии"+" "+getFioStudent()+" сдал сессию № "+getNumberSession()+
-                ", не сданно зачетов - "+getCountOfNonHandedOffsets()+" и количество экзаменов на сл. сессию - "+getCountOfExamsForNextSession());
-    }
+            System.out.println("сессия №:");
+            setNumberSession(scanner.nextInt());    
+            System.out.println("количество не сданных зачётов:");
+            setCountOfNonHandedOffsets(scanner.nextInt());
+            System.out.println("количество экзаменов на сл. сессию:");
+            setСountOfExamsForNextSession(scanner.nextInt());    
+        }
+        public int getNumberSession(){
+            return numberSession;
+        }
+        public void setNumberSession(int number){
+            this.numberSession = number;
+        }
+        public int getCountOfNonHandedOffsets(){
+            return countOfNonHandedOffsets;
+        }
+        public void setCountOfNonHandedOffsets(int count){
+            this.countOfNonHandedOffsets = count;
+        }
+        public int getCountOfExamsForNextSession(){
+            return countOfExamsForNextSession;
+        }
+        public void setСountOfExamsForNextSession(int count){
+            this.countOfExamsForNextSession = count;
+        }
+        public void print(){
+            System.out.print("Студент по фамилии"+" "+getFioStudent()+" сдал сессию № "+getNumberSession()+
+                    ", не сданно зачетов - "+getCountOfNonHandedOffsets()+" и количество экзаменов на сл. сессию - "+getCountOfExamsForNextSession());
+        }
     }
 }
--- src/chapter5/recordbook/main/Main.java
+++ src/chapter5/recordbook/main/Main.java
@@ -14,5 +14,5 @@ public class Main {
     public static void main(String[] args) {
         RecordBook recordBook=new RecordBook();
         recordBook.info.print();
-}
+    }   
 } 
